d8142e6ff51e5c96c36c53ac27964c485a1c3483
Santiago s-code2 <santiagoln09@gmail.com>
2023-01-25 00:42:57 -0500

    Card component started

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <div className='card'>
+      <img src="" alt="logo_card" />
+      <span>4041 5913 0619 5075</span>
+      <div>
+        <p>Mr.X</p>
+        <p>02/27</p>
+      </div>
+    </div>
+  )
+}

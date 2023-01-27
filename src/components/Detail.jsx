de6ea84d307849e106548999480d2ead14b270c3
Santiago s-code2 <santiagoln09@gmail.com>
2023-01-26 22:15:11 -0500

    form for cards started

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -9,10 +9,40 @@ export const Detail = () => {
 
   return (
     <section className='detail-of-card'>      
-      <h2>
+      {/* <h2>
         Hola de nuevo, <br /> {user}
       </h2>
-      <p className='no-card-selected'>Seleciona una tarjeta para ver mas detalles</p>
+      <p className='no-card-selected'>Seleciona una tarjeta para ver mas detalles</p> */}
+
+
+      <form action="">
+
+        <label htmlFor="card-holder">
+          Nombre de usario
+        </label>
+        <input type="text" placeholder='Jhon Doe' id="card-holder" />
+
+
+        <label htmlFor="card-number">
+          Numero de tarjeta
+        </label>
+        <input type="text" placeholder='0000 0000 0000 0000' id="card-number"/>
+
+        <div className='card-expiration'>
+          <label htmlFor="expiration-date">MM/YY</label>
+          <input type="text" placeholder='MM' id="expiration-date"/>
+          <input type="text" placeholder='YY' />
+        </div>
+
+        <label htmlFor="card-cvc">
+          CVC
+        </label>
+        <input type="text" name="" id="card-cvc" />
+
+        
+
+      </form>
+
     </section>
   )
 }

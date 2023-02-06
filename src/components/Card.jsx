2859686adf7567797a47b916fbe9cc4fb4400807
Santiago s-code2 <santiagoln09@gmail.com>
2023-02-06 17:44:07 -0500

    Service to select and edit a card started

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,18 @@ import premium_logo from '../assets/icons/card_icon_1.svg'
 import gold_logo from '../assets/icons/card_icon_2.svg'
 import platinum_logo from '../assets/icons/card_icon_3.svg'
 import { AuthContext } from '../context/auth/AuthContext';
+import { CardContext } from '../context/cards/CardContext';
 import './styles/card.css'
 
 export const Card = ({ type, number, expiration }) => {
 
   const { authState: { user } } = React.useContext(AuthContext);
+  const { dispatch } = React.useContext(CardContext)
+
+  const setCardToEdit = () =>{
+    /* dispatch({type, number, expiration}) */
+    console.log('To complete...')
+  }
 
   return (
     <>
--- src/context/cards/CardProvider.jsx
+++ src/context/cards/CardProvider.jsx
@@ -1,13 +1,20 @@
 import { useReducer } from "react"
+import { types } from "../../types/types"
 import { CardContext } from "./CardContext"
 import { cardReducer, initialCardState } from "./cardReducer"
 
-export const CardProvider = ({children}) => {
-  
-  const [cardState, dispatch] = useReducer(cardReducer,initialCardState)
+export const CardProvider = ({ children }) => {
+
+  const [cardState, dispatch] = useReducer(cardReducer, initialCardState)
+
+
+  const setSelectedCard = (card) => ({
+    type: types.selectCard,
+    payload: card
+  })
 
   return (
-    <CardContext.Provider value={{cardState,dispatch}}>
+    <CardContext.Provider value={{ cardState, dispatch }}>
       {children}
     </CardContext.Provider>
   )
